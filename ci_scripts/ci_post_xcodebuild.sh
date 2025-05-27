820afbd2045f006d6bd4223def5a7c426257bf79
Michael Logothetis <mmlogothetis@gmail.com>
2025-05-27 22:58:27 +1000

    Fix CI scripts

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,12 +5,26 @@
 #
 #  Created by Michael Logothetis on 26/5/2025.
 #  
+
+rm -rf docsData
+
+echo "Building DocC documentation for ModularSlothCreator..."
+
 xcodebuild -project ../MySwiftTestPackage.xcodeproj -derivedDataPath docsData -scheme MySwiftTestPackage -destination 'platform=iOS Simulator,name=iPhone 16' -parallelizeTargets docbuild
 
 echo "Copying DocC archives to doc_archives..."
 mkdir doc_archives
 cp -R `find docsData -type d -name "*.doccarchive"` doc_archives
 
-$(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path MySwiftTestPackage/$ARCHIVE_NAME --output-path docs/$ARCHIVE_NAME
+mkdir docs
+
+for ARCHIVE in doc_archives/*.doccarchive; do
+    cmd() {
+        echo "$ARCHIVE" | awk -F'.' '{print $1}' | awk -F'/' '{print tolower($2)}'
+    }
+    ARCHIVE_NAME="$(cmd)"
+    echo "Processing Archive: $ARCHIVE"
+    $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path MySwiftTestPackage/$ARCHIVE_NAME --output-path docs/$ARCHIVE_NAME
+done
 
 ./ci_site_deploy.sh
